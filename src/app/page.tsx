473463c579fe991a9045d2a40d1e1a91c5abc6d4
Ali Sina Sultani <alisiansultani255@gmail.com>
2025-07-07 06:40:41 -0400

    Theme is installed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
+import ThemeToggle from "@/components/shared/theme-toggle";
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to the course</h1>
+    <div className="p-5">
+      <div className="flex gap-x-5 justify-end ">
+        <ThemeToggle />
+      </div>
+      <h1 className="text-blue-500 font-barlow">Home page</h1>
     </div>
   );
 }
